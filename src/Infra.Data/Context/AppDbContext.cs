2b06c9a69e18aa31f699c6b73cb5db71541e4bd1
ismaelnt <ismaelnt97@gmail.com>
2023-01-20 08:37:52 -0300

    feat: generate initial migration

--- src/Infra.Data/Context/AppDbContext.cs
+++ src/Infra.Data/Context/AppDbContext.cs
@@ -1,3 +1,4 @@
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infra.Data.Context;
@@ -7,6 +8,10 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
+
+    public DbSet<Institution> Institutions { get; set; }
+    public DbSet<Position> Positions { get; set; }
+    public DbSet<Theme> Themes { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
